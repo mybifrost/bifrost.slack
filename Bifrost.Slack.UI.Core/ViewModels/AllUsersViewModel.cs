2ea432b0e34e2da8dac7680cb3d498637e81c04a
Tony Sullivan <tony.f.sullivan@gmail.com>
2015-07-10 10:43:33 -0400

    Missed a few files in the last commit - this adds the views and view models for viewing all users and user details.

---
+++ Bifrost.Slack.UI.Core/ViewModels/AllUsersViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Bifrost.Slack.Core;
+using Bifrost.Slack.Core.Users;
+using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bifrost.Slack.UI.Core.ViewModels
+{
+    public class AllUsersViewModel : MvxViewModel
+    {
+        private ISlackClient _slack;
+        public AllUsersViewModel(ISlackClient slack)
+        {
+            _slack = slack;
+        }
+
+        #region Public Methods
+        /// <summary>
+        /// Initializes the view model.
+        /// </summary>
+        /// <remarks>
+        /// This gets called by the MVVMCross framework as part of the default view model lifecycle.
+        /// </remarks>
+        public override void Start()
+        {
+            base.Start();
+            LoadAsync().ConfigureAwait(false);
+        }
+        #endregion
+
+        #region Properties
+        private ObservableCollection<User> _users;
+        public ObservableCollection<User> Users
+        {
+            get { return _users; }
+            set { SetProperty(ref _users, value); }
+        }
+
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get { return null; }
+            set
+            {
+                if (value != null)
+                {
+                    ShowViewModel<UserDetailsViewModel>(new UserDetailsParameters { UserId = value.Id });
+                }
+            }
+        }
+        #endregion
+
+        #region Private Helpers
+        /// <summary>
+        /// Loads user data from Slack and populates the Users property.
+        /// </summary>
+        private async Task LoadAsync()
+        {
+            var userData = await _slack.Users.GetAllUsersAsync();
+
+            var collection = new ObservableCollection<User>();
+            
+            // if Slack returned users, create viewmodels for them and add them to Users
+            if (userData != null)
+            {
+                foreach(var user in userData)
+                {
+                    collection.Add(user);
+                }
+            }
+
+            Users = collection;
+        }
+        #endregion
+    }
+}
---
+++ Bifrost.Slack.UI.Core/ViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Bifrost.Slack.Core;
+using Bifrost.Slack.Core.Users;
+using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bifrost.Slack.UI.Core.ViewModels
+{
+    /// <summary>
+    /// Navigation parameters that can be passed to the ShowViewModel call
+    /// when requesting the UserDetailsViewModel.
+    /// </summary>
+    public class UserDetailsParameters : IUserId
+    {
+        public string UserId { get; set; }
+        public string GetUserId() { return UserId; }
+    }
+
+    public class UserDetailsViewModel : MvxViewModel
+    {
+        private User _user;
+        private ISlackClient _slack;
+        public UserDetailsViewModel(ISlackClient slack)
+        {
+            _slack = slack;
+        }
+
+        #region Public Methods
+        /// <summary>
+        /// Initializes the view model and populates the specified Slack User.
+        /// </summary>
+        /// <param name="parameters">Initialization parameters, including the user ID to be loaded.</param>
+        /// <remarks>This is called by the MVVMCross framework if an instance of
+        /// UserDetailsParameters was provied in the ShowViewModel navigation call.</remarks>
+        public void Init(UserDetailsParameters parameters)
+        {
+            Load(parameters).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Populates the view model for the given Slack User.
+        /// </summary>
+        /// <param name="user">User that the view model represents.</param>
+        public void Initialize(User user)
+        {
+            _user = user;
+            RealName = _user.RealName;
+        }
+        #endregion
+
+        #region Properties
+        private string _realName;
+        public string RealName
+        {
+            get { return _realName; }
+            set { SetProperty(ref _realName, value); }
+        }
+        #endregion
+
+        /// <summary>
+        /// Loads the user from Slack.
+        /// </summary>
+        /// <param name="id">User ID to be loaded.</param>
+        private async Task Load(IUserId id)
+        {
+            var user = await _slack.Users.GetUserAsync(id);
+            if (user != null)
+            {
+                Initialize(user);
+            }
+        }
+    }
+}
