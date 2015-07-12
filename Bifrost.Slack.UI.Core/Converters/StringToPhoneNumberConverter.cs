ef79863a723e064d029deba5b7599280eabd9816
Tony Sullivan <tony.f.sullivan@gmail.com>
2015-07-12 13:04:01 -0400

    Adding a string to phone number converter to apply a common phone number format when displaying profiles.
    This converter is not Windows-dependant, putting it in the UI.Core project so it could be used on other platforms as well (android/iOS)

---
+++ Bifrost.Slack.UI.Core/Converters/StringToPhoneNumberConverter.cs
@@ -0,0 +1,46 @@
+﻿using Cirrious.CrossCore.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bifrost.Slack.UI.Core.Converters
+{
+    public class StringToPhoneNumberConverter : MvxValueConverter<string, string>
+    {
+        protected override string Convert(string value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // do nothing for null or empty strings
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            // pull out non-digit characters from the string
+            var digits = value.ToCharArray().Where(c => c >= '0' && c <= '9');
+            var digitsStr = new string(digits.ToArray<char>());
+
+            // build the string, checking the length before appending each segment
+            var builder = new StringBuilder();
+            
+            if (digitsStr.Length > 0)
+            {
+                builder.Append('(');
+                builder.Append(digitsStr.Substring(0, 3));
+                builder.Append(')');
+            }
+            if (digitsStr.Length > 3)
+            {
+                builder.Append(' ');
+                builder.Append(digitsStr.Substring(3, 3));
+            }
+            if (digitsStr.Length > 6)
+            {
+                builder.Append('-');
+                builder.Append(digitsStr.Substring(6, 4));
+            }
+
+            // return the formatted string
+            return builder.ToString();
+        }
+    }
+}
---
+++ Bifrost.Slack.UI.WinPhone/Converters/NativeStringToPhoneNumberConverter.cs
@@ -0,0 +1,14 @@
+﻿using Bifrost.Slack.UI.Core.Converters;
+using Cirrious.CrossCore.WindowsCommon.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bifrost.Slack.UI.WinPhone.Converters
+{
+    public class NativeStringToPhoneNumberConverter : MvxNativeValueConverter<StringToPhoneNumberConverter>
+    {
+    }
+}
